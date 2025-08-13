55f9a69bb034c65b8a974d47f34d10ed98e619d1
juanse217 <juansebas08004@gmail.com>
2025-08-12 20:54:07 -0500

    First commit, class 2

---
+++ src/main/java/com/devsenior2/modulo_3/Clase1.java
@@ -0,0 +1,5 @@
+package com.devsenior2.modulo_3;
+
+public class Clase1 {
+    //Simplemente se hablo del funcionamiento de Maven y su instalacion.    
+}
---
+++ src/main/java/com/devsenior2/modulo_3/clase2/Clase2.java
@@ -0,0 +1,7 @@
+package com.devsenior2.modulo_3.clase2;
+
+public class Clase2 {
+    public static void main(String[] args) {
+        
+    }
+}
